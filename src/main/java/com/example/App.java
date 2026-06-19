367b5c76ca071c42b007a0baae6519f19547137d
bitcsedevops06 <bitcsedevops06@gmail.com>
2026-06-19 12:20:52 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,12 +2,19 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class App 
 {
     public static void main( String[] args )
     {
-      WebDriver driver=new ChromeDriver();
+      ChromeOptions op=new ChromOptions();
+        op.addArguments("--headless");
+        op.addArguments("--no-sandbox");
+        op.addArguments("--display-dev-shm-usage");
+        
+      WebDriver driver=new ChromeDriver(op);
       driver.get("https://www.saucedemo.com");
       driver.manage().window().maximize();
       driver.findElement(By.id("user_name"));
